b825ed6e275aced39c9e1c97eb67eea733074c80
Conan Wang <v-donglwang@microsoft.com>
2023-02-24 16:22:31 +0800

    update

---
+++ - 习题/数组去重.js
@@ -0,0 +1,2 @@
+// 编写一个程序来查找字符串数组中的重复项。
+// string[] strArray = ["Sunday"， "Monday"， "Tuesday"， "Wednesday"， "Sunday"， "Monday"];
---
+++ 3- JS/- Array常用方法.js
@@ -0,0 +1,22 @@
+/**
+ 数组常用方法:
+    
+    基础:
+        shift()
+        unShift()
+        pop()
+        push()
+        sort()
+        reverse()   反转
+        splice(start,length)
+    迭代:
+        map((item,index)=>{} )
+        forEach((item,index)=>{})
+        reduce((total,pre,next)=>{},init)
+
+        find()
+        findIndex()
+        some()
+        every()
+        filter()
+ */
